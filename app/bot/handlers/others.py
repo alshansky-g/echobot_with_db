f72bce7a34582bfbc4394e131073c6fbd44d0fac
Gleb Alshanskiy <rgcs.rgcs@gmail.com>
2025-07-31 13:53:41 +0300

    feat: add user and other handlers

---
+++ app/bot/handlers/others.py
@@ -0,0 +1,13 @@
+from aiogram import Router
+from aiogram.types import Message
+from psycopg import AsyncConnection
+
+others_router = Router()
+
+
+@others_router.message()
+async def send_echo(message: Message, conn: AsyncConnection, i18n: dict):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text=i18n.get('no_echo'))
