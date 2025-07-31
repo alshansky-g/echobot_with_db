fad74d37fc239ffcdbd4feffdc38ab2113feaa64
Gleb Alshanskiy <rgcs.rgcs@gmail.com>
2025-07-31 18:31:34 +0300

    feat: add middlewares

---
+++ app/bot/middlewares/statistics.py
@@ -0,0 +1,34 @@
+import logging
+from collections.abc import Awaitable, Callable
+from typing import Any
+
+from aiogram import BaseMiddleware
+from aiogram.types import Update, User
+from psycopg import AsyncConnection
+
+from app.infrastructure.database.db import add_user_activity
+
+logger = logging.getLogger(__name__)
+
+
+class ActivityCounterMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[Update, dict[str, Any]], Awaitable[Any]],
+        event: Update,
+        data: dict[str, Any],
+    ) -> Any:
+        user: User = data.get("event_from_user")
+        if user is None:
+            return await handler(event, data)
+
+        result = await handler(event, data)
+
+        conn: AsyncConnection = data.get("conn")
+        if conn is None:
+            logger.error("No database connection found in middleware data")
+            raise RuntimeError(
+                "Missing database connection for activity logging"
+            )
+        await add_user_activity(conn, user_id=user.id)
+        return result
